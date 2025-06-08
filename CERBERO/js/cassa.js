defb9028516ddfe546102bad8569b5b4421acad4
NeRO <39014277+tytus44@users.noreply.github.com>
2025-06-08 23:09:22 +0200

    Delete CERBERO directory

--- CERBERO/js/cassa.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    window.ricaricaDatiPagina = () => console.log("Ricarico dati Cassa");
-});
-window.esportaDatiPagina = () => ({ pagina: 'cassa', timestamp: new Date().toISOString(), dati: {} });
