993ec5ba9982a74d611030412d27e4bfd95c3b13
NeRO <39014277+tytus44@users.noreply.github.com>
2025-07-14 11:02:25 +0200

    Add files via upload
    Bug fixes

--- js/index.js
+++ js/index.js
@@ -79,8 +79,7 @@ function getFormattedTimestamp() {
     const day = String(now.getDate()).padStart(2, '0');
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    return `${year}${month}${day}_${hours}${minutes}${seconds}`;
+    return `${year}${month}${day}-${hours}${minutes}`;
 }
 /* ===== FINE UTILITY ===== */
 
